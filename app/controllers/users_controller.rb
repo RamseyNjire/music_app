b4383d3866a0a81e05867d9f0d30ecbdb1071aed
Ramsey Njire <ramseynjire@gmail.com>
2021-06-23 15:11:06 +0300

    switch database to postgres

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
