a73d066d3fc133021e92795d3ac6a7e50f9d2e8e
Ramsey Njire <ramseynjire@gmail.com>
2021-06-23 16:03:26 +0300

    create partials for signup and login forms, as well as login action in session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+
+        if user.nil?
+            render json: 'Email or password is incorrect'
+        else
+            render json: "Successful login, #{user.username}!"
+        end
+    end
 end
