35bf6cb8782e07edd8f819be77b2f496aa5592b4
Ramsey Njire <ramseynjire@gmail.com>
2021-06-23 16:07:45 +0300

    add user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         if user.nil?
             render json: 'Email or password is incorrect'
         else
-            render json: "Successful login, #{user.username}!"
+            redirect_to user_url(user)
         end
     end
 end
