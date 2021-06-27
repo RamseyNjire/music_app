e495dd0deb39cd18d81b718932b07481d1815660
Ramsey Njire <ramseynjire@gmail.com>
2021-06-27 16:16:24 +0300

    write destroy method in bands controller and add delete button to band show page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -38,6 +38,14 @@ class BandsController < ApplicationController
         render :show
     end
 
+    def destroy
+        @band = Band.find_by(id: params[:id])
+
+        @band.destroy
+
+        redirect_to bands_url
+    end
+
     private
 
     def band_params
