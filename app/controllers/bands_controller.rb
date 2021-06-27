48808efb7b540a1066b5a6eef6da148f74eca0d6
Ramsey Njire <ramseynjire@gmail.com>
2021-06-27 15:40:49 +0300

    write create method in bands controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -8,9 +8,25 @@ class BandsController < ApplicationController
     def new
     end
 
+    def create
+        @band = Band.new(band_params)
+
+        if @band.save
+            redirect_to band_url(@band)
+        else
+            render json: @band.errors.full_messages
+        end
+    end
+
     def show
         @band = Band.find_by(id: params[:id])
 
         render :show
+    end
+
+    private
+
+    def band_params
+        params.require(:band).permit(:name)
     end
 end
