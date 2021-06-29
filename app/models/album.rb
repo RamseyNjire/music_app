c5c48417788cb57c1523abd0253feca1bd99afba
Ramsey Njire <ramseynjire@gmail.com>
2021-06-29 11:12:28 +0300

    finish writing validations for album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,21 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  year       :string           not null
+#  album_type :string           default("STUDIO"), not null
+#  band_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Album < ApplicationRecord
   ALBUM_TYPE = ["LIVE", "STUDIO"]
 
   validates :title, presence: { message: "^Album must have a name" }
   validates :year, presence: { message: "^What year was this album published?" }
-  # validates :album_type, inclusion: { in: }
+  validates :album_type, inclusion: { in: ALBUM_TYPE,
+                                      message: "^Please specify the album type" }
   belongs_to :band
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  year       :string           not null
+#  album_type :string           default("STUDIO"), not null
+#  band_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
