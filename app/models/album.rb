a2ecc384688208c45309fbff92fdb5a40b2356fe
Ramsey Njire <ramseynjire@gmail.com>
2021-06-29 11:03:19 +0300

    bump puma version to 4.3.8 to fix security vulnerability

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,8 @@
 class Album < ApplicationRecord
+  ALBUM_TYPE = ["LIVE", "STUDIO"]
+
+  validates :title, presence: { message: "^Album must have a name" }
+  validates :year, presence: { message: "^What year was this album published?" }
+  # validates :album_type, inclusion: { in: }
   belongs_to :band
 end
