42d8caca6337ee4222bb04ab969783e510135e0c
Ramsey Njire <ramseynjire@gmail.com>
2021-06-29 14:10:38 +0300

    write index method in albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,9 @@
 class AlbumsController < ApplicationController
+    def index
+        if params.has_key?(:band_id)
+            @albums = Album.where(band_id: params[:band_id])
+        else
+            @albums = Album.all
+        end
+    end
 end
