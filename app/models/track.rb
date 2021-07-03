605721916d451318b07cc02a645740c3bb142dc9
Ramsey Njire <ramseynjire@gmail.com>
2021-07-03 14:08:10 +0300

    write model-level validations and associations for tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,12 @@
 class Track < ApplicationRecord
+  TRACK_TYPE = ["REGULAR", "BONUS"]
+
+  validates :title, presence: { message: "^Track must have a title" }
+  validates :ord, presence: { message: "^Track must have a track number" }
+  validates :track_type, inclusion: { in: TRACK_TYPE,
+                                      message: "^Track type must be regular or bonus" }
+
+
   belongs_to :album
   belongs_to :band
 end
