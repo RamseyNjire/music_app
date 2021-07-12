9b22bdc42ad39a98b42b5af384ea27b1e796b6ed
Ramsey Njire <ramseynjire@gmail.com>
2021-07-12 20:58:47 +0300

    build helper to add musical notes to track lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def auth_token
-        "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\"/>".html_safe
+        "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{h(form_authenticity_token)}\"/>".html_safe
     end
 
     def delete_method
@@ -9,6 +9,22 @@ module ApplicationHelper
 
     def patch_method
         "<input type=\"hidden\" name=\"_method\" value=\"patch\"/>".html_safe
+    end
+
+    def ugly_lyrics(lyrics)
+        
+        safe_lyrics = h(lyrics).chars.map do |char|
+            if char == "\n"
+                char + "&#9835; "
+            else
+                char
+            end
+        end.join("")
+
+        html = "<pre>"
+        html += safe_lyrics
+        html += "</pre>"
+        html.html_safe
     end
 end
 
