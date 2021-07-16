f74dbd4d887ed8050e72559f9061e6ebccb7d206
Ramsey Njire <ramseynjire@gmail.com>
2021-07-16 15:33:19 +0300

    finish validations and associations tests for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    username { 'Caligula' }
+    email { 'caligula@example.com' }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe "validations" do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:session_token) }
+    
+    it do
+      should validate_presence_of(:password_digest).
+      with_message("^Password cannot be blank")
+    end
+
+    it do
+      should validate_uniqueness_of(:username).
+      with_message("^Username already taken")
+    end
+
+    it do
+      should validate_uniqueness_of(:email).
+      with_message("^Email already taken")
+    end 
+  end
+
+  describe "associations" do
+    it { should have_many(:notes).dependent(:destroy) }
+  end
 end
