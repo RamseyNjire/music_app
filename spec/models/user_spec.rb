537d30f5c689a535364ff03b70fe37798b2aed00
Ramsey Njire <ramseynjire@gmail.com>
2021-07-16 15:55:37 +0300

    write test for #reset_session_token method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,9 +30,20 @@ RSpec.describe User, type: :model do
 
   describe "#is_password?" do
     it "returns true when password matches" do
-      expect{ user.is_password?("password").to eq(true) }
+      expect{ user.is_password?("password").to be_true }
     end
 
-    
+    it "returns false when password does not match" do
+      expect{ user.is_password?("not_password").to be_false }
+    end
+  end
+
+  describe "#reset_session_token" do
+    it "changes the session token when called" do
+      old_session_token = user.session_token
+      new_session_token = user.reset_session_token
+
+      expect(new_session_token).not_to eq(old_session_token)
+    end
   end
 end
